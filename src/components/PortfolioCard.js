53e81000e0c54221b977d3405be51158f68794c7
Josh Glugatch <joshglugatch@gmail.com>
2020-11-17 18:21:29 -0800

    map through project info in portfolio

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Projects from "../projects.json"
 
 
 function Navbar() {
@@ -18,12 +18,17 @@ function Navbar() {
               <p style={{fontSize: '10px'}}>*Not all projects supported on mobile devices</p>
               <hr />
               <section className="row mb-4">
-                <figure className="col-lg-4"><p style={{fontFamily: '"Bebas Neue"', fontSize: '24px'}}>PetSpace <a href="https://github.com/joshglugatch/PetSpace" target="_blank"><img src="./assets/github-logo-black.png" alt="repository" style={{width: '25px', marginLeft: '10px'}} /></a></p><a href="https://petspace2020.herokuapp.com/" target="_blank"><img src="./assets/FilteredPets.gif" alt="petSpace" className="float-left img-thumbnail p-0" /></a></figure>
-                <figure className="col-lg-4"><p style={{fontFamily: '"Bebas Neue"', fontSize: '24px'}}>o'Clocktail <a href="https://github.com/joshglugatch/o-clocktail" target="_blank"><img src="./assets/github-logo-black.png" alt="repository" style={{width: '25px', marginLeft: '10px'}} /></a></p><a href="https://joshglugatch.github.io/o-clocktail/" target="_blank"><img src="./assets/oclocktaildemo.png" alt="o-clocktail" className="float-left img-thumbnail p-0" /></a></figure>
-                <figure className="col-lg-4"><p style={{fontFamily: '"Bebas Neue"', fontSize: '24px'}}>Eat-Da-Burger<a href="https://github.com/joshglugatch/Eat-Da-Burger" target="_blank"><img src="./assets/github-logo-black.png" alt="repository" style={{width: '25px', marginLeft: '10px'}} /></a></p><a href="https://eat-da-burger-yum.herokuapp.com/" target="_blank"><img src="./assets/eat-da-burger-screenshot.png" alt="burgerapp" className="float-left img-thumbnail p-0" /></a></figure>
-                <figure className="col-lg-4"><p style={{fontFamily: '"Bebas Neue"', fontSize: '24px'}}>Avatar Memory Game<a href="https://joshglugatch.github.io/Team-Avatar-Clicky-Game/" target="_blank"><img src="./assets/github-logo-black.png" alt="repository" style={{width: '25px', marginLeft: '10px'}} /></a></p><a href="https://joshglugatch.github.io/Team-Avatar-Clicky-Game/" target="_blank"><img src="./assets/clickgamedemo.gif" alt="clickgame" className="float-left img-thumbnail p-0" /></a></figure>
-                <figure className="col-lg-4"><p style={{fontFamily: '"Bebas Neue"', fontSize: '24px'}}>Weather Dashboard<a href="https://github.com/joshglugatch/weather-dashboard-page" target="_blank"><img src="./assets/github-logo-black.png" alt="repository" style={{width: '25px', marginLeft: '10px'}} /></a></p><a href="https://joshglugatch.github.io/weather-dashboard-page/" target="_blank"><img src="./assets/weatherdemo.png" alt="weatherapp" className="float-left img-thumbnail p-0" /></a></figure>
-                <figure className="col-lg-4"><p style={{fontFamily: '"Bebas Neue"', fontSize: '24px'}}>Day Planner<a href="https://github.com/joshglugatch/day-planner" target="_blank"><img src="./assets/github-logo-black.png" alt="repository" style={{width: '25px', marginLeft: '10px'}} /></a></p><a href="https://joshglugatch.github.io/day-planner/" target="_blank"><img src="./assets/dayscheduledemo.png" alt="dayplanner" className="float-left img-thumbnail p-0" /></a></figure>
+
+                    {Projects.map(project=>{
+                        return(
+                <figure className="col-lg-4"><p style={{fontFamily: '"Bebas Neue"', fontSize: '24px'}}>{project.title} <a href={project.repo} rel="noreferrer" target="_blank">
+                <img src="./assets/github-logo-black.png" alt="" style={{width: '25px', marginLeft: '10px'}} /></a></p>
+                <a href={project.deploy} rel="noreferrer" target="_blank">
+                <img src={project.image} alt={project.title} className="float-left img-thumbnail p-0" /></a></figure>
+                        )
+
+                    })}
+               
               </section>
             </section>
           </section>
