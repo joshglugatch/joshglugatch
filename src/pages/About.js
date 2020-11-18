c404974b555ffbe4ac4d2d1e9b6aba02bb98b23c
Josh Glugatch <joshglugatch@gmail.com>
2020-11-17 20:19:53 -0800

    remove unnecessary link import

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar"
 import AboutCard from "../components/AboutCard"
 import Footer from "../components/Footer"
-import { Link } from "react-router-dom";
+
 
 
 class About extends React.Component {
