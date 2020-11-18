47d29665ac51e1de1d5ef2df90f9e8c33ab0bb07
Josh Glugatch <joshglugatch@gmail.com>
2020-11-17 20:19:38 -0800

    fix footer target_blank error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,9 @@ function Footer() {
     <section className="card width-full" style={{backgroundColor: 'black', borderTop: 'whitesmoke solid 8px', height: '100px'}}>
       <article className="row">
         <div className="card-body container text-center">
-          <a href="https://www.linkedin.com/in/joshua-glugatch/" target="_blank"><img src="./assets/linkedinlogo.png" style={{width: '40px', marginRight: '30px', marginLeft: '50px'}} alt="linkedinlogo" /></a>
-          <a href="https://github.com/joshglugatch" target="_blank"><img src="./assets/githublogo.png" style={{width: '40px', marginRight: '30px'}} alt="githublogo" /></a>
-          <a href="https://www.instagram.com/joshglug/?hl=en" target="_blank"><img src="./assets/instagram.png" style={{width: '40px', marginRight: '30px'}} alt="instagramlogo" /></a>
+          <a href="https://www.linkedin.com/in/joshua-glugatch/" target="_blank" rel="noreferrer"><img src="./assets/linkedinlogo.png" style={{width: '40px', marginRight: '30px', marginLeft: '50px'}} alt="linkedinlogo" /></a>
+          <a href="https://github.com/joshglugatch" target="_blank" rel="noreferrer"><img src="./assets/githublogo.png" style={{width: '40px', marginRight: '30px'}} alt="githublogo" /></a>
+          <a href="https://www.instagram.com/joshglug/?hl=en" target="_blank" rel="noreferrer"><img src="./assets/instagram.png" style={{width: '40px', marginRight: '30px'}} alt="instagramlogo" /></a>
         </div>
       </article>
     </section>
